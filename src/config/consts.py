d4d4698d8f88b3cff56346dd3326adef4e77246b
dmunoz2023-source <dmunoz2023@alu.uct.cl>
2025-11-26 08:00:59 -0300

    feat: add utility

---
+++ src/config/consts.py
@@ -0,0 +1,26 @@
+GLOBAL_FONTS = {
+    "h1": {"size": 18, "weight": "bold"},
+    "h2": {"size": 14, "weight": "bold"},
+    "h3": {"size": 12, "weight": "bold"},
+    "total_price": {"size": 20, "weight": "bold"},
+    "btn_primary": {"size": 14, "weight": "bold"},
+    "btn_menu_item": {"size": 12, "weight": "bold"},
+    "label_normal": {"size": 14},
+    "label_important": {"size": 14, "weight": "bold"},
+}
+
+MENU_IMAGES = {
+    "Papas Fritas": "./images/papas_fritas.png",
+    "Completo": "./images/completo.png", 
+    "Hamburguesa": "./images/hamburguesa.png",
+    "Pepsi": "./images/pepsi.png", 
+    "Panqueques": "./images/panqueques.png", 
+    "Pollo Frito": "./images/pollo_frito.png", 
+    "Ensalada Mixta": "./images/ensalada_mixta.png"
+}
+
+MENU_COLUMNS = ['nombre', 'precio']
+
+STOCK_COLUMNS = ["nombre", "unidad", "cantidad"]
+
+ORDER_COLUMNS = ["Nombre del Menu", "Cantidad", "Precio Unitario", "Subtotal"]
